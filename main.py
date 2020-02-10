b76477b3d79b56f29a521ce7642c8842ae386148
RevolutionsStudio <60020572+RevolutionsStudio@users.noreply.github.com>
2020-02-10 21:06:36 +0100

    Added command test
    yes verry much the test

--- main.py
+++ main.py
@@ -1,30 +1,76 @@
+
+# Code by Cypooos - Twitter: @cypooos - Revolutions STUDIO 2020
+
+# /----------------------
+# | Importation
+# \----------------------
+
+
 import sys, os
+import shlex
 
 def echo(*args):
-    print(*args)
-    sys.stdout.flush()
+  print(*args)
+  sys.stdout.flush()
 
 echo("--- App Started ---")
 
 import asyncio, discord
-client = discord.Client()
+CLIENT = discord.Client()
 
-import DiscordCommandLineGenerator
-CommandLine = DiscordCommandLineGenerator.CommandLine()
+import CommandLineGenerator
+CommandLine = CommandLineGenerator.CommandLine()
 
 
+# /----------------------
+# | Commands of bot
+# \----------------------
 
-@client.event
+
+@CommandLine.addFunction
+def ping(**kwargs) -> "ping":
+  """
+It just answer pong.
+  """
+  return "pong !"
+
+# /----------------------
+# | Bot
+# \----------------------
+
+
+def setToDiscordCommandLine(CL,name="discordCommand"):
+
+  def foo(self,message):
+    commandes = message.content.split(";")
+    self.vars
+    for commande in commandes:
+      trt_commande = shlex.split(commande)
+      self.cmdReturn = self.__command(trt_commande)
+    if self.cmdReturn != None: return self.cmdReturn
+
+  setattr(CL, name, foo)
+
+# Setup the command-line to be Discord compatible
+setToDiscordCommandLine(CommandLine,"discordCommand")
+
+@CLIENT.event
 @asyncio.coroutine
-def on_message(message):
-    echo(message)
+async def on_message(message):
+  echo(message.content)
+  await CLIENT.send_message(message.channel, CommandLine.discordCommand(message))
+
+
+# /----------------------
+# | Start
+# \----------------------
 
 
 try:
-    token = os.environ['TOKEN']
+  token = os.environ['TOKEN']
 except KeyError:
-    # Not on server
-    echo("Quit because TOKEN not found.")
-    exit()
+  # Not on server
+  echo("Quit because TOKEN not found.")
+  exit()
 
-client.run(token)
+CLIENT.run(token)
