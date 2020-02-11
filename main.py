43e242725bc679016cf46b22e83d0725adfcdc10
RevolutionsStudio <60020572+RevolutionsStudio@users.noreply.github.com>
2020-02-11 09:19:25 +0100

    yes no
    no yes

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def notification(name:str,**kwargs) -> "notif (Rasylium|Revolutions|Rideos|Rogem
   '''Vous donne le role de notification
 	'''
   name = name.lower()
-  message = CL.message
+  message = CommandLine.message
   if name == "all":
     for x in CONF.roles.values():
       message.author.add_roles(discord.utils.get(message.author.server.roles, id=x))
